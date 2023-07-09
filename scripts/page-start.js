4d84dddec18eaac5abe82992dac3b71bc9449a29
Marcel Kirchner <marcel_kv@hotmail.com>
2023-07-10 00:01:52 +0200

    Added order page

--- scripts/page-start.js
+++ scripts/page-start.js
@@ -36,3 +36,47 @@ function initCustomersList(customers, orders) {
         list.appendChild(listItem);
     });
 }
+
+function handleCustomerClick(customer, orders){
+    pageManager.showOrderPage();
+
+    const order = orders?.find(order => order.customer.firstName === customer.firstName && order.customer.lastName === customer.lastName);
+    if (order) {
+        const table = document.getElementById('selectedCustomerProducts');
+        const tbody = table.querySelector('tbody');
+        tbody.innerHTML = '';
+
+        order.productOrders.forEach(function (productOrder) {
+            const row = document.createElement('tr');
+
+            const name = document.createElement('td');
+            const cost = document.createElement('td');
+            const quantity = document.createElement('td');
+            const total = document.createElement('td');
+            name.textContent = productOrder.product.name;
+            cost.textContent = productOrder.product.cost + " €";
+            quantity.textContent = productOrder.quantity;
+            total.textContent = productOrder.cost + " €";
+            name.classList.add('firstCol');
+
+            row.appendChild(name);
+            row.appendChild(cost);
+            row.appendChild(quantity);
+            row.appendChild(total);
+
+            tbody.appendChild(row);
+        });
+
+        const total = document.getElementById('selectedCustomerTotal');
+        total.innerHTML = order.cost + " €";
+        const selectedCustomerName = document.getElementById('selectedCustomerName');
+        selectedCustomerName.innerHTML = customer.firstName + " " + customer.lastName;
+    }
+
+    const backButton = document.getElementById('backButton');
+    backButton.onclick = function () { onBackClick() }
+}
+
+function onBackClick(){
+    pageManager.showCustomersPage();
+}
--- scripts/page.js
+++ scripts/page.js
@@ -7,14 +7,27 @@ class PageManager {
         return document.getElementsByClassName('page customers')[0];
     }
 
+    get orderPage() {
+        return document.getElementsByClassName('page order')[0];
+    }
+
     showStartPage() {
         this.hideElement(this.customersPage);
+        this.hideElement(this.orderPage);
         this.showElement(this.startPage);
     }
 
     showCustomersPage(){
         this.hideElement(this.startPage);
+        this.hideElement(this.orderPage);
         this.showElement(this.customersPage);
+    }
+
+    showOrderPage() {
+        console.log(this.startPage);
+        this.hideElement(this.startPage);
+        this.hideElement(this.customersPage);
+        this.showElement(this.orderPage);
     }
 
     hideElement(element) {
