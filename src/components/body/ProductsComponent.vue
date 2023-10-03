9af12065c7bd0a27618053517a9de583f62618c3
Marcel Kirchner <marcel_kv@hotmail.com>
2023-10-03 14:36:06 +0700

    Refactored products responsiveness

--- src/components/body/ProductsComponent.vue
+++ src/components/body/ProductsComponent.vue
@@ -4,9 +4,9 @@ import {
   computed,
   defineComponent,
   inject,
-  nextTick,
-  onMounted,
+  onBeforeMount,
   ref,
+  watch,
 } from "vue";
 import { Product } from "@/common/models/product";
 import { IResponsiveService } from "@/common/services/responsive-service.interface";
@@ -22,44 +22,70 @@ export default defineComponent({
     },
   },
   setup() {
+    const ref_products = ref(null);
+    const isOneColumn = ref(true);
+    const isMoreThanOneColumn = ref(false);
     const responsiveService = inject<IResponsiveService>("responsiveService");
     const clientService = inject<IClientService>("clientService");
     const isLoading = ref(true);
     const products = ref<Product[]>([]);
-    const productMaxWidth = 400;
-    const productMinWidth = 200;
-    const productMargin = 20;
-    const productPadding = 10;
 
-    const widthSize = computed(() => responsiveService.widthSize.value);
-    const productsListStyle = computed(() => {
-      return {
-        "--maxWidth": productMaxWidth + "px",
-        "--minWidth": productMinWidth + "px",
-        "--margin": productMargin + "px",
-        "--padding": productPadding + "px",
-      };
-    });
-
-    const isLargerThanProduct = computed(() => {
-      const totalProductSize = productMaxWidth + productMargin;
-      return (
-        totalProductSize < responsiveService.windowWidth.value &&
-        responsiveService.widthSize.value !== SizeType.Large
-      );
-    });
-
-    onMounted(async () => {
-      await nextTick();
+    onBeforeMount(async () => {
       const loadedProducts = await clientService.getProductsAsync();
       products.value.splice(0, loadedProducts.length, ...loadedProducts);
       isLoading.value = false;
+      setTimeout(() => onWidthChanged(), 0);
     });
 
+    const widthSize = computed(() => responsiveService.widthSize.value);
+
+    function onWidthChanged(): void {
+      const { listWidth, productWidth } = getWidths();
+
+      if (listWidth > productWidth * 2 + 2) {
+        isOneColumn.value = false;
+        isMoreThanOneColumn.value = true;
+      } else if (responsiveService.widthSize.value !== SizeType.ExtraSmall) {
+        isOneColumn.value = true;
+        isMoreThanOneColumn.value = false;
+      } else {
+        isOneColumn.value = false;
+        isMoreThanOneColumn.value = false;
+      }
+    }
+
+    function getWidths(): { listWidth: number; productWidth: number } {
+      const listElement = ref_products.value as HTMLDivElement | null;
+      const defaultWidths = { listWidth: 0, productWidth: 0 };
+
+      if (!listElement) {
+        return defaultWidths;
+      }
+
+      const elements = listElement.getElementsByClassName("product");
+
+      if (!elements || elements.length < 1) {
+        return defaultWidths;
+      }
+
+      const firstElement = elements[0];
+      const computedStyles = window.getComputedStyle(firstElement);
+      const marginLeft = parseFloat(computedStyles.marginLeft);
+      const marginRight = parseFloat(computedStyles.marginRight);
+      const border = parseFloat(computedStyles.borderWidth);
+      const listWidth = listElement.clientWidth;
+      const productWidth =
+        firstElement.clientWidth + marginLeft + marginRight + border * 2;
+      return { listWidth, productWidth };
+    }
+
+    watch(() => responsiveService.windowWidth.value, onWidthChanged);
+
     return {
+      ref_products,
       widthSize,
-      productsListStyle,
-      isLargerThanProduct,
+      isOneColumn,
+      isMoreThanOneColumn,
       products,
       isLoading,
     };
@@ -68,16 +94,15 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="products">
+  <div class="products" ref="ref_products">
     <Spinner v-if="isLoading" v-bind:withText="true"></Spinner>
     <div
       v-else
       class="productsList"
       v-bind:class="{
-        isLargerThanProduct: isLargerThanProduct,
-        isLarge: widthSize === SizeType.Large,
+        isOneColumn: isOneColumn,
+        isMoreThanOneColumn: isMoreThanOneColumn,
       }"
-      v-bind:style="productsListStyle"
     >
       <ProductCard
         v-for="product in products"
@@ -101,16 +126,21 @@ export default defineComponent({
   overflow: auto;
 
   .productsList {
+    --maxWidth: 400px;
+    --minWidth: 200px;
+    --margin: 20px;
+    --padding: 10px;
+
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
 
-    &.isLargerThanProduct {
+    &.isOneColumn {
       align-items: center;
     }
 
-    &.isLarge {
+    &.isMoreThanOneColumn {
       flex-direction: row;
       flex-wrap: wrap;
     }
